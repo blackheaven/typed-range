f40041f25998d77b46853dd37f4c7913108b49ff
Robert Massaioli <robertmassaioli@gmail.com>
2013-06-08 16:57:17 +1000

    Have the range code in a state.
    But I want to refactor it. It is getting much to hard to reason about.

--- Data/Range/Range.hs
+++ Data/Range/Range.hs
@@ -14,20 +14,37 @@ import Data.List (sortBy)
 data Range a
    = SingletonRange a
    | SpanRange a a
+   | InfiniteRange
    deriving(Eq, Show)
 
+infiniteRange :: Range a
+infiniteRange = SpanRange Nothing Nothing
+
 rangesOverlap :: (Ord a) => Range a -> Range a -> Bool
+-- rangesOverlap InfiniteRange _ = True
+-- rangesOverlap _ InfiniteRange = True
 rangesOverlap (SingletonRange a) (SingletonRange b) = a == b
 rangesOverlap (SingletonRange a) (SpanRange x y) = isBetween a (x, y)
 rangesOverlap (SpanRange x y) (SingletonRange a) = isBetween a (x, y)
-rangesOverlap (SpanRange x y) (SpanRange a b) = isBetween x (a, b) || isBetween a (x, y)
+rangesOverlap (SpanRange Nothing Nothing) _ = True -- Infinite ranges overlap
+rangesOverlap _ (SpanRange Nothing Nothing) = True
+rangesOverlap (SpanRange _ Nothing) (SpanRange _ Nothing) = True -- Ranges same direction
+rangesOverlap (SpanRange Nothing _) (SpanRange Nothing _) = True
+rangesOverlap (SpanRange Nothing (Just y)) (SpanRange (Just x) Nothing) = x <= y
+rangesOverlap (SpanRange (Just x) Nothing) (SpanRange Nothing (Just y)) = x <= y
+rangesOverlap (SpanRange (Just x) (Just y)) (SpanRange (Just a) (Just b)) 
+   = isBetween x (Just a, Just b) || isBetween a (Just x, Just y)
+
+-- x- y- will both overlap
+-- 3- -9 will overlap
+-- x- -y will overlap if x <= y
 
 inRange :: (Ord a) => Range a -> a -> Bool
 inRange (SingletonRange a) value = value == a
 inRange (SpanRange x y) value = isBetween value (x, y)
 
-isBetween :: (Ord a) => a -> (a, a) -> Bool
-isBetween a (x, y) = x <= a && a <= y
+isBetween :: (Ord a) => a -> (Maybe a, Maybe a) -> Bool
+isBetween a (x, y) = maybe True (<= a) x && maybe True (a <=) y
 
 mergeRange :: (Ord a) => Range a -> Range a -> Either (Range a, Range a) (Range a)
 mergeRange r1 r2 = if rangesOverlap r1 r2
@@ -37,10 +54,35 @@ mergeRange r1 r2 = if rangesOverlap r1 r2
       assumeMerge :: (Ord a) => Range a -> Range a -> Range a
       assumeMerge (SingletonRange _) x = x
       assumeMerge x (SingletonRange _) = x
-      assumeMerge (SpanRange x y) (SpanRange a b) = SpanRange (min x a) (max y b)
+      assumeMerge (SpanRange x y) (SpanRange a b) = SpanRange (minMaybe x a) (maxMaybe y b)
+
+      minMaybe :: (Ord a) => Maybe a -> Maybe a -> Maybe a
+      minMaybe = min
+
+      maxMaybe :: (Ord a) => Maybe a -> Maybe a -> Maybe a
+      maxMaybe Nothing _ = Nothing
+      maxMaybe _ Nothing = Nothing
+      maxMaybe x y = max x y 
+
+flipOrdering :: Ordering -> Ordering
+flipOrdering LT = GT
+flipOrdering GT = LT
+flipOrdering EQ = EQ
+
+-- The longer the range the earlier that it should go in the pipeline.
+orderRanges :: (Ord a) => Range a -> Range a -> Ordering
+orderRanges (SingletonRange x) (SingletonRange y) = compare x y
+orderRanges _ (SpanRange Nothing _) = GT
+orderRanges (SpanRange Nothing _) _ = LT
+orderRanges (SingletonRange x) (SpanRange (Just y) _) = compare x y
+orderRanges (SpanRange (Just x) _) (SingletonRange y) = compare x y
+orderRanges (SpanRange (Just x) _) (SpanRange (Just y) _) = compare x y
+
+sortRanges :: (Ord a) => [Range a] -> [Range a]
+sortRanges = sortBy orderRanges
 
 mergeRanges :: (Ord a) => [Range a] -> [Range a]
-mergeRanges ranges = mergeRangesHelper $ sortBy (comparing getLowest) ranges
+mergeRanges = mergeRangesHelper . sortRanges
    where
       mergeRangesHelper :: (Ord a) => [Range a] -> [Range a]
       mergeRangesHelper (x:y:xs) = case mergeRange x y of
@@ -48,14 +90,14 @@ mergeRanges ranges = mergeRangesHelper $ sortBy (comparing getLowest) ranges
                                      Right a -> mergeRangesHelper (a:xs)
       mergeRangesHelper xs = xs
 
-      getLowest :: (Ord a) => Range a -> a
-      getLowest (SingletonRange x) = x
-      getLowest (SpanRange x _) = x
-
 fromRanges :: (Ord a, Enum a) => [Range a] -> [a]
 fromRanges [] = []
-fromRanges (SingletonRange x:xs) = x : fromRanges xs
-fromRanges (SpanRange a b:xs) = [a..b] ++ fromRanges xs
+fromRanges (x:xs) = (case x of
+   SingletonRange x -> [x] 
+   SpanRange Nothing _ -> error "Cannot generate an infinite range."
+   SpanRange _ Nothing -> error "Cannot generate an infinite range."
+   SpanRange (Just a) (Just b) -> [a..b]
+   ) ++ fromRanges xs
 
 fromMergedRanges :: (Ord a, Enum a) => [Range a] -> [a]
 fromMergedRanges = fromRanges . mergeRanges
