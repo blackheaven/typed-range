6394f8b8a5a67856c793ba771cc3c59e1d5d90f1
Robert Massaioli <robertmassaioli@gmail.com>
2013-06-30 22:58:56 +1000

    Fully documented the parser.

--- Data/Range/Parser.hs
+++ Data/Range/Parser.hs
@@ -1,5 +1,12 @@
 {-# LANGUAGE FlexibleContexts #-}
 
+-- | It should not be unexpected that you will be given a string representation of some
+-- ranges and you will need to parse them so that you can then do some further processing.
+-- This parser exists in order to make the most common forms of range strings easy to
+-- parse. It does not cover all cases however but you should not be too worried about
+-- that because you should be able to write your own parser using parsec or Alex/Happy and
+-- then you can convert everything that you parse into a RangeTree object for easier
+-- processing.
 module Data.Range.Parser 
    ( parseRanges
    , ranges
@@ -12,43 +19,34 @@ import Text.Parsec.String
 
 import Data.Range.Range
 
-{-
- - I want to be able to parse the following formats into ranges
- - * Single numbers
- - * Spans with separating characters
- - * Infinite wildcard ranges
- - * Infinite in one direction ranges
- -
- - Then I want to be able to parse arbitrary ranges.
- -}
-
+-- | The arguments that are used, and can be modified, while parsing a standard range
+-- string.
 data RangeParserArgs = Args 
-   { unionSeparator :: String
-   , intersectionSeparator :: String
-   , rangeSeparator :: String
-   , wildcardSymbol :: String
+   { unionSeparator :: String -- ^ A separator that represents a union.
+   , rangeSeparator :: String -- ^ A separator that separates the two halves of a range.
+   , wildcardSymbol :: String -- ^ A separator that implies an unbounded range.
    }
    deriving(Show)
 
--- Break them into token first with Alex and then parse them into ranges with Happy. Make
--- it so that Alex is configurable and different characters can be used as tokens. I
--- wonder if it is even possible to do this in a smart way? If we cannot do this then use
--- parsec. Maybe it would be better to use parsec from the start.
-
+-- | These are the default arguments that are used by the parser. Please feel free to use
+-- the default arguments for you own parser and modify it from the defaults at will.
 defaultArgs :: RangeParserArgs 
 defaultArgs = Args
    { unionSeparator = ","
    , rangeSeparator = "-"
-   , intersectionSeparator = "&" -- this one seems wrong, work on it
    , wildcardSymbol = "*"
    }
 
+-- | Given a string this function will either return a parse error back to the user or the
+-- list of ranges that are represented by the parsed string.
 parseRanges :: (Read a) => String -> Either ParseError [Range a]
 parseRanges = parse (ranges defaultArgs) "(range parser)"
 
 string_ :: Stream s m Char => String -> ParsecT s u m ()
 string_ x = string x >> return ()
 
+-- | Given the parser arguments this returns a parser that is capable of parsing a list of
+-- ranges.
 ranges :: (Read a) => RangeParserArgs -> Parser [Range a]
 ranges args = range `sepBy` (string $ unionSeparator args)
    where 
