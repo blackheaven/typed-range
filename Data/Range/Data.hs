a74b44a692eab982a4ba5bb0d54d2d001067433f
Robert Massaioli <robertmassaioli@gmail.com>
2013-06-30 22:51:10 +1000

    Started writing documentation for the code.
    We are getting closer and closer all of the time to a library that can be released to the
    public.

--- Data/Range/Data.hs
+++ Data/Range/Data.hs
@@ -1,11 +1,13 @@
--- This module contains all of the public data structures that need to be avaliable to
--- people wanting to use the code.
+-- | The Data module for common data types within the code.
 module Data.Range.Data where
 
+-- | The Range Data structure; it is capable of representing any type of range. This is
+-- the primary data structure in this library. Everything should be possible to convert
+-- back into this datatype. All ranges in this structure are inclusively bound.
 data Range a
-   = SingletonRange a
-   | SpanRange a a
-   | LowerBoundRange a
-   | UpperBoundRange a
-   | InfiniteRange
+   = SingletonRange a      -- ^ Represents a single element as a range.
+   | SpanRange a a         -- ^ Represents a bounded and inclusive range of elements.
+   | LowerBoundRange a     -- ^ Represents a range with only an inclusive lower bound.
+   | UpperBoundRange a     -- ^ Represents a range with only an inclusive upper bound.
+   | InfiniteRange         -- ^ Represents an infinite range over all values.
    deriving(Eq, Show)
